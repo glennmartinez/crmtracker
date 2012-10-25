60477223a7f8051f60d4079ec704c1efc3ec7ce3
Glenn Martin <glennmartin76@glenns-MacBook-Air-2.local>
2012-10-26 09:14:58 +1100

    updated labouritem controller

--- app/controllers/labouritems_controller.rb
+++ app/controllers/labouritems_controller.rb
@@ -27,6 +27,31 @@ def create
 	
 end
 
+def update
+      @labouritem = Labouritem.find(params[:id])
+	  @quote = Quote.find(params[:quote_id])
+	 	# @quote.update_attributes(params[:note])
+	respond_to do |format|
+      if @labouritem.update_attributes(params[:labouritem])
+        format.html { redirect_to @labouritem, notice: 'quote was successfully created.' }
+        format.json { render json: @labouritem, status: :created, location: @labouritem }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @labouritem.errors, status: :unprocessable_entity }
+      end
+    end
+	
+end
+
+def destroy
+    @labouritem = Labouritem.find(params[:id])
+    @labouritem.destroy
+
+    # respond_to do |format|
+    #   format.html { redirect_to quotes }
+    #   format.json { head :no_content }
+    # end
+end
 
 def find_quote
 	@quote = Quote.find(params[:quote_id])
